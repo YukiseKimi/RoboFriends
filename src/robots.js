f1cbef4ec799592b2c3d66f51d90067ac2f9b66a
YukiseKimi <Mannychau@gmail.com>
2018-11-01 17:46:49 -0400

    first commit

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Zura",
+        username: 'Katsura',
+        email: 'KatsuraJaNai@gmail.com'
+    },
+    {
+        id: 2,
+        name: "Gintoki",
+        username: 'Gintoki',
+        email: 'Yorozuya@gmail.com'
+    },
+    {
+        id: 3,
+        name: "Prince Baka",
+        username: 'BakaPrince',
+        email: 'BakaPrince@gmail.com'
+    },
+    {
+        id: 4,
+        name: "Zura",
+        username: 'Katsura',
+        email: 'KatsuraJaNai@gmail.com'
+    },
+    {
+        id: 5,
+        name: "Gintoki",
+        username: 'Gintoki',
+        email: 'Yorozuya@gmail.com'
+    },
+    {
+        id: 6,
+        name: "Prince Baka",
+        username: 'BakaPrince',
+        email: 'BakaPrince@gmail.com'
+    },
+    {
+        id: 7,
+        name: "Zura",
+        username: 'Katsura',
+        email: 'KatsuraJaNai@gmail.com'
+    },
+    {
+        id: 8,
+        name: "Gintoki",
+        username: 'Gintoki',
+        email: 'Yorozuya@gmail.com'
+    },
+    {
+        id: 9,
+        name: "Prince Baka",
+        username: 'BakaPrince',
+        email: 'BakaPrince@gmail.com'
+    },
+    {
+        id: 10,
+        name: "Zura",
+        username: 'Katsura',
+        email: 'KatsuraJaNai@gmail.com'
+    },
+    {
+        id: 11,
+        name: "Gintoki",
+        username: 'Gintoki',
+        email: 'Yorozuya@gmail.com'
+    },
+    {
+        id: 12,
+        name: "Prince Baka",
+        username: 'BakaPrince',
+        email: 'BakaPrince@gmail.com'
+    },
+];
